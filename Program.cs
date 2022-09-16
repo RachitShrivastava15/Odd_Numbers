0af1bbbf8a3fcab2d97e1a13f925531177474e64
RachitShrivastava <rachitshrivastava15@gmail.com>
2022-09-16 14:54:20 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace OddNumbers
+{
+    class Program
+    {
+        public static void Main(string [] args)
+        {
+            int n = 100;
+            for (int i = 1; i <=n ; i++)
+            {
+                if(i % 2 != 0 )
+                {
+                    Console.WriteLine (i);
+                }
+            }
+        }
+    }
+}
